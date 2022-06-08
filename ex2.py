5fe2013b19c09b80ca71f4e7b7353916adf00a16
alex-billiard <107134972+alex-billiard@users.noreply.github.com>
2022-06-08 19:22:28 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,21 @@
+import math
+
+
+
+
+
+
+a2 = 10
+b2 = 3
+z2 = math.factorial(a2)/(math.factorial(b2)*(math.factorial(a2 - b2)))
+p2 = 1 / z2 * 100
+p2 = round(p2, 1)
+print(f"Ответ на задачу 2: {p2} процента(ов)")
+
+
+
+
+
+
+
+
